6fdf204d574621485882cfb080b0f029a490153e
Mahmoud Sharshera <43113784+zixa5824@users.noreply.github.com>
2024-02-04 22:05:22 +0200

    Hackerrank Challenges

---
+++ hackerrank/lib/hackerrank.ex
@@ -0,0 +1,18 @@
+defmodule Hackerrank do
+  @moduledoc """
+  Documentation for `Hackerrank`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hackerrank.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

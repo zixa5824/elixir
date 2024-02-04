8e99c884442d7ec2987e9b06ea643e7cf04f7bca
Mahmoud Sharshera <43113784+zixa5824@users.noreply.github.com>
2024-02-04 23:29:37 +0200

    Challenge 1,2,3 done

--- hackerrank/lib/hackerrank.ex
+++ hackerrank/lib/hackerrank.ex
@@ -1,6 +1,7 @@
 defmodule Hackerrank do
   #Purpose of this module is to store basic code snippets that was learnt from HackerRank site
-  # C1 : Challenge One
+
+  # C1 : Challenge One____________
   def hackerrankC1() do
     a = IO.gets("") |> String.trim
     b = IO.gets("") |> String.trim
@@ -8,5 +9,21 @@ defmodule Hackerrank do
     b = String.to_integer(b)
     IO.puts(a+b)
   end
+  #------------------------------------
+
+  # C2 : Challenge Two____________
+  defp times(0), do: nil
+  defp times(n) do
+      IO.puts("Hello World")
+      times(n-1)
+  end
+  def run() do
+    IO.gets("")
+    |> String.trim()
+    |> String.to_integer()
+    |> times()
+  end
+  #Hackerrank.run() should be outside of the module if you want it to run immediately after compilling inside hackerrank
+  #------------------------------------
 
 end

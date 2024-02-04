e9f2451e2f880aa6864419170b36dfcfa3f54bb8
Mahmoud Sharshera <43113784+zixa5824@users.noreply.github.com>
2024-02-04 22:16:37 +0200

    C1 easy

--- hackerrank/lib/hackerrank.ex
+++ hackerrank/lib/hackerrank.ex
@@ -1,18 +1,12 @@
 defmodule Hackerrank do
-  @moduledoc """
-  Documentation for `Hackerrank`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Hackerrank.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  #Purpose of this module is to store basic code snippets that was learnt from HackerRank site
+  # C1 : Challenge One
+  def hackerrankC1() do
+    a = IO.gets("") |> String.trim
+    b = IO.gets("") |> String.trim
+    a = String.to_integer(a)
+    b = String.to_integer(b)
+    IO.puts(a+b)
   end
+
 end
